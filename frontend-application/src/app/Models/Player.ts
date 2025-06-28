31a7271cfbbc76e715b1cd68d6aa48b86b4670c5
DenisUreke <70695931+DenisUreke@users.noreply.github.com>
2025-06-28 17:25:38 +0200

    Player class changes

--- frontend-application/src/app/Models/Player.ts
+++ frontend-application/src/app/Models/Player.ts
@@ -5,18 +5,24 @@ export class Player {
   public is_leader: boolean;
   public player_score: number;
   public player_lives: number;
+  public team_selection_position: number;
+  public is_in_game: boolean;
 
   constructor(
     name: string, 
     player_number: number = 0, 
     is_leader: boolean = false,
     player_score: number = 0,
-    player_lives: number = 0
+    player_lives: number = 0,
+    team_selection_position: number = 1,
+    is_in_game = false
   ) {
     this.player_name = name;
     this.player_number = player_number;
     this.is_leader = is_leader;
     this.player_score = player_score;
     this.player_lives = player_lives;
+    this.team_selection_position = team_selection_position;
+    this.is_in_game = is_in_game;
   }
 }
